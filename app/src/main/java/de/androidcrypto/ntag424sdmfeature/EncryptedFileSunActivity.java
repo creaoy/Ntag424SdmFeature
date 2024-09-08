16e3fa0221ae7e97c17b563e5cf9821dd405720f
Johnny Kartakov <creaoy@gmail.com>
2024-09-08 15:51:11 +0700

    add default activity and #id=1

--- app/src/main/java/de/androidcrypto/ntag424sdmfeature/EncryptedFileSunActivity.java
+++ app/src/main/java/de/androidcrypto/ntag424sdmfeature/EncryptedFileSunActivity.java
@@ -314,7 +314,7 @@ public class EncryptedFileSunActivity extends AppCompatActivity implements NfcAd
                     writeToUiAppend(output, "File 02h Writing the NDEF URL Template SUCCESS");
                     //System.out.println("*** NdefRecord: " + new String(ndefRecord, StandardCharsets.UTF_8));
                     // write the timestamp data (19 characters long + 5 characters '#1234'
-                    byte[] fileData = (getTimestampLog() + "#1234").getBytes(StandardCharsets.UTF_8);
+                    byte[] fileData = "#1".getBytes(StandardCharsets.UTF_8);
                     try {
                         if (isLrpAuthenticationMode) {
                             WriteData.run(dnaC, NDEF_FILE_NUMBER, fileData, (87 + 16)); // LRP Encrypted PICC data is 16 bytes longer
